26db875370c3b34031894997cc1c4e278cc0ecda
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-08-03 11:34:28 +0000

    Add mongo configuration.

--- src/main/java/com/mc/McMongoProviderApplication.java
+++ src/main/java/com/mc/McMongoProviderApplication.java
@@ -2,8 +2,14 @@ package com.mc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.mc.service")
 public class McMongoProviderApplication {
 
 	public static void main(String[] args) {
